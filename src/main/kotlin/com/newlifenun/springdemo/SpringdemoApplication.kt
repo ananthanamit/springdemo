9b30c64ec5bf34b109546bf2b10659da7233e451
ananthanamit <anan.thanamit@gmail.com>
2024-08-05 15:13:14 +0700

    Updated files and fixed bugs

--- src/main/kotlin/com/newlifenun/springdemo/SpringdemoApplication.kt
+++ src/main/kotlin/com/newlifenun/springdemo/SpringdemoApplication.kt
@@ -1,21 +1,10 @@
 package com.newlifenun.springdemo
 
-import com.newlifenun.springdemo.controllers.DemoController
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SpringdemoApplication
-
-//class HelloController{
-//	@GetMapping("/")
-//	fun hello():String {
-//		return "Hello Spring Boot!!"
-//	}
-//}
 
 fun main(args: Array<String>) {
 	runApplication<SpringdemoApplication>(*args)
